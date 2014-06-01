1806218983a2475337e28db4ad42585d7f6d2ef2
viktoria97 <viktoria97@users.noreply.github.com>
2014-06-01 23:33:54 +0300

    Update game.c

--- Board/game.c
+++ Board/game.c
@@ -8,12 +8,12 @@ int main() {
 	int AI, FULL; //FULL flags the AI bot when the board has no empty slots
 	char ch;
 
-	generate_deck();
+	
 	game_begin(&board, &side, &AI);
 	while( !winner(board) ) {
 		turn_begin(&board.pl[side]);
-		on_turn(&board, AI, side);
-		improved_turn_end(&board, AI, &side);
+		
+	
 	}
 	print_winner(board);
 	return 0;
